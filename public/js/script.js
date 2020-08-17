82339d30dd234f119f32bd28b807cda2b2876113
kotzabasi <kotzabasi@hotmail.com>
2020-08-17 05:08:09 +0300

    change background regarding weather

--- public/js/script.js
+++ public/js/script.js
@@ -1,41 +1,79 @@
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+
 messageOne = document.querySelector('#message-1')
 messageTwo = document.querySelector('#message-2')
-
-
-
-
 
 var place = weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
     const location = search.value
-
+    
     messageOne.textContent = ''
-    messageTwo.textContent= 'Loading...'
+    messageTwo.textContent = 'Loading...'
 
-    fetch(`/weather?address=${location}`).then((response)=>{
-        response.json().then((data)=>{
-            if(data.error){
-                messageOne.textContent=data.error
+    fetch(`/weather?address=${location}`).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error
                 messageTwo.textContent = ''
             }
-            else{
-                messageTwo.innerHTML = data.location+" "+ "<br>"+"Temperature: "+data.forecast.temperature+
-                "<br>"+"Description: "+data.forecast.description+"<br>"+"Feels Like: "+data.forecast.feelLike+"<br>"+
-                "Humidity: "+data.forecast.humidity
-                // console.log(data.forecast)
+            else {
+                messageTwo.innerHTML = data.location + " " + "<br>" + "Temperature: " + data.forecast.temperature +
+                    "<br>" + "Description: " + data.forecast.description + "<br>" + "Feels Like: " + data.forecast.feelLike + "<br>" +
+                    "Humidity: " + data.forecast.humidity
+            }
+            var description = data.forecast.description.toLowerCase()
+            if (description.includes('clear')) {
+                document.querySelector('.main').style.backgroundImage = 'url(../img/clear.jpg)'
+                document.getElementById('link1').style.color = 'white'
+                document.getElementById('link2').style.color = 'white'
+                document.getElementById('link3').style.color = 'white'
+            }
+            else if (description.includes('sunny')) {
+                document.querySelector('.main').style.backgroundImage = 'url(../img/sunny.jpg)'
+                document.getElementById('link1').style.color = '#728C00'
+                document.getElementById('link2').style.color = '#728C00'
+                document.getElementById('link3').style.color = '#728C00'
+            }
+            else if (description.includes('cloudy') || description.includes('overcast')) {
+                document.querySelector('.main').style.backgroundImage = 'url(../img/cloudy.jpg)'
+                document.getElementById('link1').style.color = 'blue'
+                document.getElementById('link2').style.color = 'blue'
+                document.getElementById('link3').style.color = 'blue'
+            }
+
+            else if (description.includes('shower')) {
+                document.querySelector('.main').style.backgroundImage = 'url(../img/shower.jpg)'
+                document.getElementById('link1').style.color = 'white'
+                document.getElementById('link2').style.color = 'white'
+                document.getElementById('link3').style.color = 'white'
+            }
+            else if (description.includes('rain')) {
+                document.querySelector('.main').style.backgroundImage = 'url(../img/rain.jpg)'
+                document.getElementById('link1').style.color = 'white'
+                document.getElementById('link2').style.color = 'white'
+                document.getElementById('link3').style.color = 'white'
+            }
+            else if (description.includes('mist') || description.includes('haze') || description.includes('fog') ) {
+                document.querySelector('.main').style.backgroundImage = 'url(../img/mist.jpg)'
+                document.getElementById('link1').style.color = 'white'
+                document.getElementById('link2').style.color = 'white'
+                document.getElementById('link3').style.color = 'white'
+            }
+            else if (description.includes('snow') || description.includes('sleet')){
+                document.querySelector('.main').style.backgroundImage = 'url(../img/snow.jpg)'
+                document.getElementById('link1').style.color = 'white'
+                document.getElementById('link2').style.color = 'white'
+                document.getElementById('link3').style.color = 'white'
+            }
+            else {
+                document.querySelector('.main').style.backgroundImage = 'url(../img/bluesky.jpg)'
             }
         })
     })
-   
-
-    
 })
 
 
 
-  
-   
